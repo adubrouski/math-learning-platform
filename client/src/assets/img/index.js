3e2972cc5843900f34875346f2f0e892679658de
Anton Dubrovskij <dubrovskij145@yandex.by>
2021-02-09 21:58:44 +0300

    update the folders structure

--- client/src/assets/img/index.js
+++ client/src/assets/img/index.js
@@ -4,5 +4,18 @@ import homeIcon from './menu/icons/homeIcon.svg';
 import teacherIcon from './menu/icons/teacherIcon.svg';
 import clipboardIcon from './menu/icons/tabletIcon.svg';
 import bookIcon from './menu/icons/bookIcon.svg';
+import loader from './loader.svg';
+import tickIcon from './exams/cross.svg';
+import crossIcon from './exams/tick.svg';
 
-export { logoIcon, manIcon, homeIcon, teacherIcon, clipboardIcon, bookIcon };
+export {
+  logoIcon,
+  manIcon,
+  homeIcon,
+  teacherIcon,
+  clipboardIcon,
+  bookIcon,
+  loader,
+  tickIcon,
+  crossIcon,
+};
