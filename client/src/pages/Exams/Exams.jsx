a5664cb439af134d4805621721463a2b3fda0951
Anton Dubrovskij <dubrovskij145@yandex.by>
2021-02-10 12:13:03 +0300

    add test list generation from the redux

--- client/src/pages/Exams/Exams.jsx
+++ client/src/pages/Exams/Exams.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Testing } from '../../components';
 
+import { fetchAllExams } from '../../redux/thunks/exams';
+
 const Tests = () => {
+  const dispatch = useDispatch();
+  const { exams } = useSelector(({ exams }) => exams);
+
+  React.useEffect(() => {
+    dispatch(fetchAllExams());
+  }, []);
+
   return (
     <div className="exams">
       <h3 className="exams__title">Пройдите тест</h3>
       <div className="exams__content">
-        {/* <ul className="exams__list">
-          <li className="exams__list-item exams__tick">test thrth rh rthgf rth rth rthrt hrth</li>
-          <li className="exams__list-item exams__cross">test thrth rh rthgf rth rth rthrt hrth</li>
-          <li className="exams__list-item">test thrth rh rthgf rth rth rthrt hrth</li>
-          <li className="exams__list-item">test thrth rh rthgf rth rth rthrt hrth</li>
-          <li className="exams__list-item">test thrth rh rthgf rth rth rthrt hrth</li>
-          <li className="exams__list-item">test thrth rh rthgf rth rth rthrt hrth</li>
-        </ul> */}
-        <Testing />
+        <ul className="exams__list">
+          {exams &&
+            Object.values(exams).map(({ exam }) => (
+              <li key={exam._id} className="exams__list-item">
+                {exam.question}
+              </li>
+            ))}
+        </ul>
+        {/* <Testing /> */}
       </div>
     </div>
   );
