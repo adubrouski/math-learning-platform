736a1c645f1ef8bd67b674b006415fcf75b02975
Anton Dubrovskij <dubrovskij145@yandex.by>
2021-02-10 18:13:58 +0300

    fix exams routes

--- server/routes/exams.js
+++ server/routes/exams.js
@@ -5,11 +5,28 @@ const router = Router();
 
 router.get('/', async (req, res) => {
   const exams = await Exams.find().exec();
-  const normalizeExams = exams[0].exams.map((exam) => ({
-    exam,
-  }));
 
+  const normalizeExams = exams[0].exams.map(({ _id, topic }) => ({
+    id: _id,
+    topic,
+  }));
   res.status(200).json({ ...normalizeExams });
 });
+
+router.get('/exam', async (req, res) => {
+  const exams = await Exams.find().exec();
+
+  const normalizeExams = exams[0].exams
+    .map(({ _id, topic, questions }) => ({
+      id: _id,
+      topic,
+      questions,
+    }))
+    .filter((item) => item.id == req.query.id);
+
+  res.status(200).json({ ...normalizeExams[0] });
+});
+
+router.post('/exams', async (req, res) => {});
 
 module.exports = router;
