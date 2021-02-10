fa63fa2b6245dbaf298c34cdafd9af85ecf90b06
Anton Dubrovskij <dubrovskij145@yandex.by>
2021-02-10 18:12:35 +0300

    add exactResults field to model

--- server/models/user.js
+++ server/models/user.js
@@ -13,6 +13,10 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  examsResults: {
+    type: Array,
+    required: true,
+  },
 });
 
 module.exports = model('User', userSchema);
