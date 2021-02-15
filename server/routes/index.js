977b373e8fe3cd2bcfc283e6808746f037fba93b
Anton Dubrovskij <dubrovskij145@yandex.by>
2021-02-15 20:14:42 +0300

    use route protect middleware

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,11 @@ const topics = require('./topics');
 const classrooms = require('./classrooms');
 const exams = require('./exams');
 
+const protectRoute = require('../middleware/protectRoute');
+
 module.exports = (app) => {
   app.use('/api/auth', auth);
-  app.use('/api/topics', topics);
-  app.use('/api/classrooms', classrooms);
-  app.use('/api/exams', exams);
+  app.use('/api/topics', protectRoute, topics);
+  app.use('/api/classrooms', protectRoute, classrooms);
+  app.use('/api/exams', protectRoute, exams);
 };
