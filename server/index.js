c06c058a9ea6c90bfca9af23a11955d337b1777b
Anton Dubrovskij <dubrovskij145@yandex.by>
2021-02-15 20:16:04 +0300

    add process variables

--- server/index.js
+++ server/index.js
@@ -1,22 +1,21 @@
 const express = require('express');
-const config = require('config');
 const cookieParser = require('cookie-parser');
 
-const PORT = config.get('port');
-const MONGO_URI = config.get('db-uri');
+require('dotenv').config();
 
 const app = express();
 
-app.use(cookieParser(config.get('cookie-secret')));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 require('./startup/cors')(app);
-require('./startup/db')(MONGO_URI);
+
+require('./startup/db')(process.env.MONGO_URI);
 
 app.use(express.json({ extended: 'true' }));
 app.use(express.urlencoded({ extended: 'true' }));
 
 require('./routes/index')(app);
 
-app.listen(PORT, () => {
-  console.log(`SERVER IS RUNNING ON ${PORT} PORT`);
+app.listen(process.env.PORT, () => {
+  console.log(`SERVER IS RUNNING ON ${process.env.PORT} PORT`);
 });
